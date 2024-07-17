5570623025a6f236756571cbe936cc6ceb59919c
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-07-17 01:37:13 +0000

    Refactor code.
    I have learnt something new in implementing helper methods today. That
    is when calling a helper method of a class in the class, you have to
    prepend it with the self keyword.
    
    Today is my first time getting to know about helper methods.

--- binary_tree.py
+++ binary_tree.py
@@ -13,48 +13,48 @@ class BinaryTree:
         if self.root is None:
             self.root = Node(data)
         else:
-            _insert(data, self.root)
+            self._insert(data, self.root)
 
     def _insert(self, data, current_node):
         if data < current_node.data:
             if current_node.left is None:
                 current_node.left = Node(data)
             else:
-                _insert(data, current_node.left)
+                self._insert(data, current_node.left)
         elif data > current_node.data:
             if current_node.right is None:
                 current_node.right = Node(data)
             else:
-                _insert(data, current_node.right)
+                self._insert(data, current_node.right)
         else:
             print(f"{data} already exists in the tree!")
 
     def in_order_traversal(self):
         if self.root is not None:
-            _in_order_traversal(self.root)
+            self._in_order_traversal(self.root)
 
     def _in_order_traversal(self, current_node):
         if current_node is not None:
-            _in_order_traversal(current_node.left)
+            self._in_order_traversal(current_node.left)
             print(f"{current_node.data}", end=" ")
-            _in_order_traversal(current_node.right)
+            self._in_order_traversal(current_node.right)
 
     def pre_order_traversal(self):
         if self.root is not None:
-            _pre_order_traversal(self.root)
+            self._pre_order_traversal(self.root)
 
     def _pre_order_traversal(self, current_node):
         if current_node is not None:
             print(f"{current_node.data}", end=" ")
-            _pre_order_traversal(current_node.left)
-            _pre_order_traversal(current_node.right)
+            self._pre_order_traversal(current_node.left)
+            self._pre_order_traversal(current_node.right)
 
     def post_order_traversal(self):
         if self.root is not None:
-            _post_order_traversal(self.root)
+            self._post_order_traversal(self.root)
 
     def _post_order_traversal(self, current_node):
         if current_node is not None:
-            _post_order_traversal(current_node.left)
-            _post_order_traversal(current_node.right)
+            self._post_order_traversal(current_node.left)
+            self._post_order_traversal(current_node.right)
             print(current_node.data, end=" ")
